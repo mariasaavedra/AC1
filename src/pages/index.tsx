63869a380581324174af2f4e0676d9dceac67f93
Maria Saavedra <msaav3@gmail.com>
2023-05-04 16:11:22 -0500

    chore: updated txt size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,71 +6,82 @@ import Falcon from '@/components/Falcon/Falcon';
 
 export default function HomePage() {
   const BG = {
-    CH_1: '/images/1.png',
-    CH_2: '/images/2.png',
-    CH_3: '/images/3.png',
+    CH__1: '/video/CH1.mp4',
+    CH__2: '/images/2.png',
+    CH__3: '/images/3.png',
   };
 
-  const [background, setBackground] = useState(BG.CH_1);
+  const [background, setBackground] = useState(BG.CH__1);
 
   return (
     <div className='absolute inset-0'>
+      <video
+        autoPlay={true}
+        muted={true}
+        className='absolute z-0'
+        width='100%'
+        height='100%'
+        controls={false}
+      >
+        <source src='/video/CH1.mp4' type='video/mp4' />
+      </video>
+
       <main
         className=' ac-grid h-full p-2 text-white '
-        style={{
-          background: `url(${background}) no-repeat center center / cover`,
-        }}
+        // style={{
+        //   background: `url(${background}) no-repeat center center / cover`,
+        // }}
       >
         {/*  header */}
-        <nav className='flex justify-between'>
-          <span>AC_1</span>
-          <span>TECHNICAL APPAREL STUDIO</span>
+        <nav className='relative flex justify-between'>
+          <span className='text-sm'>AC__1</span>
+          <span className='text-sm'>TECHNICAL APPAREL STUDIO</span>
         </nav>
-        <article className='flex  flex-col '>
+        <article className='relative flex  flex-col '>
           {/* chapters */}
-          <ul className='my-16 mt-12 flex justify-center py-8'>
+          <ul className='relative my-16 mt-12 flex justify-center py-8'>
             <li
-              onClick={() => setBackground(BG.CH_1)}
+              onClick={() => setBackground(BG.CH__1)}
               className='m-4 cursor-pointer'
             >
-              CH_1
+              CH__1
             </li>
             <li
-              onClick={() => setBackground(BG.CH_2)}
+              onClick={() => setBackground(BG.CH__2)}
               className='m-4 cursor-pointer'
             >
-              CH_2
+              CH__2
             </li>
             <li
-              onClick={() => setBackground(BG.CH_3)}
+              onClick={() => setBackground(BG.CH__3)}
               className='m-4 cursor-pointer'
             >
-              CH_3
+              CH__3
             </li>
           </ul>
           {/* sign-up */}
           <section className='my-16 flex flex-col text-center'>
-            <span className='my-8'>SIGN UP FOR ACCESS</span>
+            <span className='my-8 text-xs'>SIGN UP FOR ACCESS</span>
             <div className='search-wrapper relative w-[80vw] max-w-sm self-center'>
               <input
-                className='w-full border-white bg-transparent text-white placeholder-white'
+                className='w-full border-white bg-transparent text-xs text-white placeholder-white'
                 type='text'
                 placeholder='ENTER E-MAIL'
               ></input>
-              <button className='absolute bottom-0 right-2 top-0 border-l-[1px] border-white px-2 pl-4 text-center text-white'>
+              <button className='absolute bottom-0 right-2 top-0 border-l-[1px] border-white px-2 pl-4 text-center text-xs text-white'>
                 JOIN
               </button>
             </div>
           </section>
-          <Link className='my-16 flex flex-col text-center' href='/#'>
+          <Link className='my-16 flex flex-col text-center text-sm' href='/#'>
             PASSWORD
           </Link>
         </article>
 
         {/* footer */}
-        <footer className='flex justify-between p-2'>
+        <footer className='z-5  relative flex justify-between p-2'>
           <Falcon />
-          <span>FOUNDERS CLUB</span>
+          <span className='text-sm'>FOUNDERS CLUB</span>
         </footer>
       </main>
     </div>
